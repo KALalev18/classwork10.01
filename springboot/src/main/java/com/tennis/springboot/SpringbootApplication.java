2cf6949db330c008cb2581863f4e9344c9678e27
KALalev18 <KALalev18@codingburgas.bg>
2023-01-10 15:42:57 +0200

    Initial commit

---
+++ springboot/src/main/java/com/tennis/springboot/SpringbootApplication.java
@@ -0,0 +1,15 @@
+package com.tennis.springboot;
+
+import java.sql.Date;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApplication.class, args);
+	}
+
+}
